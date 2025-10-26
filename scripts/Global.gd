c115817fb377b01c02a88d63a0460696dd5af868
Tanner Hamilton <tannerhamilton2000@gmail.com>
2025-10-26 16:40:35 -0700

    only the export

--- scripts/Global.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-var player_color : Color = Color.WHITE
-
-func set_player_color(new_color: Color) -> void:
-	player_color = new_color
-	print("Player color set globally to: ", player_color)
--- scripts/simple_topo.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		$"..".game_over()
-	pass # Replace with function body.
