0a2ea4a5465e80fb78f7887e8d51988f963bfb1c
Tanner <tannerhamilton2000@gmail.com>
2025-11-30 10:39:37 -0800

    added leaderboard code

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -4,3 +4,14 @@ var player_color : Color = Color.WHITE
 func set_player_color(new_color: Color) -> void:
 	player_color = new_color
 	print("Player color set globally to: ", player_color)
+
+func _ready():
+	SilentWolf.configure({
+	"api_key": "bRIszXPRBdOvk0jN3SFO3QZjWZDY8OAaOZvq3oK7",
+	"game_id": "SpaceLander",
+	"log_level": 1
+	})
+
+	SilentWolf.configure_scores({
+	"open_scene_on_close": "res://scenes/MainPage.tscn"
+	})
